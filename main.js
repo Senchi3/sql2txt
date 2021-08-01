09959c74071cf40ccb3747654d1b43131e7207ea
Senchi3 <sensensenchi@gmail.com>
2021-08-01 01:45:19 -0500

    Thanks em

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const db = new sqlite3.Database('./db/blocdenotas.db');
 //Using SQLite each() Method Instead of forEach()
 db.each("SELECT title, body, date FROM notes",
     (error, row) => {
-        let data = row.date + " " + row.title + " " + row.body;
+        let data = row.date + " - \'" + row.title + "\'\n\n" + row.body;
 
         // let parsedTitle = row.title.replace(/\//g, "-");
         // let parsedTitle = row.title.replace("[^a-zA-Z0-9\\.\\\ -]", "_");
